ab45d212bde85f0474c6682b8f5bbacb2605d665
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2014-07-11 02:11:51 +0400

    [nuodb] Wrap clean shutdown to start-hook.sh

--- nuodb_2.0.4/start-hook.sh
+++ nuodb_2.0.4/start-hook.sh
@@ -5,3 +5,7 @@ chown -R nuodb:nuodb /data/nuodb
 /etc/init.d/nuowebconsole start
 /etc/init.d/nuoautoconsole start
 /bin/bash
+/etc/init.d/nuoautoconsole stop
+/etc/init.d/nuowebconsole stop
+/etc/init.d/nuoagent stop
+/etc/init.d/ssh stop
