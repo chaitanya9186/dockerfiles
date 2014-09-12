c5a6a9ed1463eadde271e1a5ceedc94cebb402f6
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2014-09-12 07:27:26 +0400

    [orientdb] Fix

--- orientdb/opt/orientdb.sh
+++ orientdb/opt/orientdb.sh
@@ -11,12 +11,13 @@ etcdctl set /cleawing/services/orientdb/$CONTAINER_NAME/http $(docker port $CONT
 etcdctl set /cleawing/services/orientdb/$CONTAINER_NAME/binary $(docker port $CONTAINER_NAME 2424)
 etcdctl set /cleawing/services/orientdb/$CONTAINER_NAME/cluster $(docker port $CONTAINER_NAME 2434)
 
-# Set exists members from etcd
-confd -verbose -node=$ETCDCTL_PEERS -onetime
-
 sed -i.bak "s/GROUP_NAME/$GROUP_NAME/" /etc/confd/templates/hazelcast.template.xml
 sed -i.bak2 "s/GROUP_PASS/$GROUP_PASS/" /etc/confd/templates/hazelcast.template.xml
-sed -i.bak3 "s/CONTAINER_NAME/$CONTAINER_NAME/" /etc/confd/templates/orientdb-dserver-config.template.xml
+sed -i.bak3 "s/CLUSTER_IP/$(docker port $CONTAINER_NAME 2434)/" /etc/confd/templates/hazelcast.template.xml
+sed -i.bak "s/CONTAINER_NAME/$CONTAINER_NAME/" /etc/confd/templates/orientdb-dserver-config.template.xml
+
+# Set exists members from etcd
+confd -verbose -node=$ETCDCTL_PEERS -onetime
 
 # smart shutdown on SIGINT and SIGTERM
 function on_exit() {
