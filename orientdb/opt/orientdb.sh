ca2c349eafdb3489f8ab688e7f003901bc5d25c6
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2014-09-12 08:48:47 +0400

    [orientdb] Fix opt/orientdb.sh

--- orientdb/opt/orientdb.sh
+++ orientdb/opt/orientdb.sh
@@ -6,14 +6,18 @@ set -eo pipefail
 GROUP_NAME=${GROUP_NAME=dev}
 GROUP_PASS=${GROUP_PASS=devpass}
 
+PUBLIC=`docker port $HOSTNAME 2480`
+BINARY=`docker port $HOSTNAME 2424`
+CLUSTER=`docker port $HOSTNAME 2434`
+
 # Self-publishing
-etcdctl set /cleawing/services/orientdb/$HOSTNAME/http $(docker port $HOSTNAME 2480)
-etcdctl set /cleawing/services/orientdb/$HOSTNAME/binary $(docker port $HOSTNAME 2424)
-etcdctl set /cleawing/services/orientdb/$HOSTNAME/cluster $(docker port $HOSTNAME 2434)
+etcdctl set /cleawing/services/orientdb/$HOSTNAME/http $PUBLIC
+etcdctl set /cleawing/services/orientdb/$HOSTNAME/binary $BINARY
+etcdctl set /cleawing/services/orientdb/$HOSTNAME/cluster $CLUSTER
 
 sed -i.bak "s/GROUP_NAME/$GROUP_NAME/" /etc/confd/templates/hazelcast.template.xml
 sed -i.bak2 "s/GROUP_PASS/$GROUP_PASS/" /etc/confd/templates/hazelcast.template.xml
-sed -i.bak3 "s/CLUSTER_IP/$(docker port $CONTAINER_NAME 2434)/" /etc/confd/templates/hazelcast.template.xml
+sed -i.bak3 "s/CLUSTER_IP/$CLUSTER/" /etc/confd/templates/hazelcast.template.xml
 sed -i.bak "s/HOSTNAME/$HOSTNAME/" /etc/confd/templates/orientdb-dserver-config.template.xml
 
 # Set exists members from etcd
