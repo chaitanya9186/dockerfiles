a924d880d10ae26264c69475fe013f27f01ff922
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2014-09-14 06:56:58 +0400

    [skip build][orientdb] Enable groups isolation

--- orientdb/opt/orientdb.sh
+++ orientdb/opt/orientdb.sh
@@ -6,6 +6,7 @@ GROUP_PASS=${GROUP_PASS=devpass}
 CLUSTER=`docker port $HOSTNAME 2434`
 
 # Prepare config
+sed -i.bak "s/GROUP_NAME/$GROUP_NAME/" /etc/confd/conf.d/hazelcast.xml.toml
 sed -i.bak "s/GROUP_NAME/$GROUP_NAME/" /etc/confd/templates/hazelcast.template.xml
 sed -i.bak2 "s/GROUP_PASS/$GROUP_PASS/" /etc/confd/templates/hazelcast.template.xml
 sed -i.bak3 "s/CLUSTER_IP/$CLUSTER/" /etc/confd/templates/hazelcast.template.xml
